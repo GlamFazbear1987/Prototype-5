daef3a1ddb22a65497adcabeec3f7e2a66e72a1d
GlamFazbear1987 <146845967+GlamFazbear1987@users.noreply.github.com>
2025-02-05 12:39:52 -0500

    5.1

--- Assets/Course Library/Scripts/Target.cs
+++ Assets/Course Library/Scripts/Target.cs
@@ -9,17 +9,41 @@ public class Target : MonoBehaviour
     float minSpeed = 12;
     float maxSpeed = 28;
     float minTorque = -10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
     float maxTorque = 10;
 
    // Start is called before the first frame update
    void Start()
    {
        targetRb = GetComponent<Rigidbody>();
-       targetRb.AddForce(Vector2.up * Random.Range(minSpeed,maxSpeed), ForceMode.Impulse);
+       targetRb.AddForce(Vector3.up * Random.Range(minSpeed,maxSpeed), ForceMode.Impulse);
        targetRb.AddTorque(Random.Range(minTorque,maxTorque),Random.Range(minTorque,maxTorque), Random.Range(minTorque,maxTorque), ForceMode.Impulse);
+       transform.position = RandomSpawnPos();
    }
 
+   Vector3 RandomForce() 
+   {
+    return Vector3.up * Random.Range(minSpeed, maxSpeed);
+   }
+   float RandomTorque() 
+   {
+    return Random.Range(-maxTorque, maxTorque);
+   }
+   Vector3 RandomSpawnPos()
+   {
+    return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+   }
 
+   private void OnMouseDown()
+   {
+    Destroy(gameObject);
+   }
+
+   private void OnTriggerEnter(Collider other)
+   {
+    Destroy(gameObject);
+   }
    // Update is called once per frame
    void Update()
    {
