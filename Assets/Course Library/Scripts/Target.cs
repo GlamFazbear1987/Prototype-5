f0a2b0f311cb65e72a9e87211df5e530fcbe8abb
GlamFazbear1987 <146845967+GlamFazbear1987@users.noreply.github.com>
2025-02-25 12:10:15 -0500

    5.2

--- Assets/Course Library/Scripts/Target.cs
+++ Assets/Course Library/Scripts/Target.cs
@@ -7,11 +7,14 @@ public class Target : MonoBehaviour
     Rigidbody targetRb;
 
     float minSpeed = 12;
-    float maxSpeed = 28;
+    float maxSpeed = 20;
     float minTorque = -10;
     private float xRange = 4;
     private float ySpawnPos = -6;
     float maxTorque = 10;
+    private GameManager gameManager;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
 
    // Start is called before the first frame update
    void Start()
@@ -20,16 +23,19 @@ public class Target : MonoBehaviour
        targetRb.AddForce(Vector3.up * Random.Range(minSpeed,maxSpeed), ForceMode.Impulse);
        targetRb.AddTorque(Random.Range(minTorque,maxTorque),Random.Range(minTorque,maxTorque), Random.Range(minTorque,maxTorque), ForceMode.Impulse);
        transform.position = RandomSpawnPos();
+       gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
    }
 
    Vector3 RandomForce() 
    {
     return Vector3.up * Random.Range(minSpeed, maxSpeed);
    }
+
    float RandomTorque() 
    {
     return Random.Range(-maxTorque, maxTorque);
    }
+
    Vector3 RandomSpawnPos()
    {
     return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
@@ -38,6 +44,8 @@ public class Target : MonoBehaviour
    private void OnMouseDown()
    {
     Destroy(gameObject);
+    Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+    gameManager.UpdateScore(pointValue);
    }
 
    private void OnTriggerEnter(Collider other)
@@ -49,5 +57,4 @@ public class Target : MonoBehaviour
    {
       
    }
-
 }
